2af74e0393c97f4f010fcb3fd08776a9a83a8f7f
Steve Yen <steve.yen@gmail.com>
2013-06-19 23:23:35 -0700

    RegisterChannel().

--- gdec.go
+++ gdec.go
@@ -1,8 +1,30 @@
 package gdec
 
+import (
+	"reflect"
+)
+
+type Lattice interface{}
+
+type Channel struct {
+	t reflect.Type
+}
+
 type D struct {
+	channels map[string]*Channel
+}
+
+func (d *D) RegisterChannel(name string, x interface{}) *Channel {
+	c := NewChannel(x)
+	d.channels[name] = c
+	return c
+}
+func NewChannel(x interface{}) *Channel {
+	return &Channel{t: reflect.TypeOf(x)}
 }
 
 func NewD() *D {
-	return &D{}
+	return &D{
+		channels: make(map[string]*Channel),
+	}
 }
--- gdec_test.go
+++ gdec_test.go
@@ -1,6 +1,7 @@
 package gdec
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -8,4 +9,54 @@ func TestNewD(t *testing.T) {
 	if NewD() == nil {
 		t.Errorf("expected D")
 	}
+}
+
+type KVPut struct {
+	ReqId      int64
+	Addr       string
+	ClientAddr string
+	Key        string
+	Val        Lattice
+}
+type KVPutResponse struct {
+	ReqId       int64
+	Addr        string
+	ReplicaAddr string
+}
+type KVGet struct {
+	ReqId      int64
+	Addr       string
+	ClientAddr string
+	Key        string
+}
+type KVGetResponse struct {
+	ReqId       int64
+	Addr        string
+	ReplicaAddr string
+	Key         string
+	Val         Lattice
+}
+
+func TestKV(t *testing.T) {
+	d := NewD()
+
+	kvputs := d.RegisterChannel("kvputs", KVPut{})
+	kvputr := d.RegisterChannel("kvput_responses", KVPutResponse{})
+	kvgets := d.RegisterChannel("kvgets", KVGet{})
+	kvgetr := d.RegisterChannel("kvget_responses", KVGetResponse{})
+
+	if kvputs == nil {
+		t.Errorf("expected non-nil channel")
+	}
+	if kvputr == nil {
+		t.Errorf("expected non-nil channel")
+	}
+	if kvgets == nil {
+		t.Errorf("expected non-nil channel")
+	}
+	if kvgetr == nil {
+		t.Errorf("expected non-nil channel")
+	}
+
+	fmt.Printf("%#v\n", d)
 }
