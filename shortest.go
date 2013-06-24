82b35f7c85115c900b4af112b05f48d0c7a579ba
Steve Yen <steve.yen@gmail.com>
2013-06-23 22:01:22 -0700

    ShortestPath example.

---
+++ shortest.go
@@ -0,0 +1,42 @@
+package gdec
+
+type ShortestPathLink struct {
+	From string
+	To   string
+	Cost int
+}
+
+type ShortestPath struct {
+	From string
+	To   string
+	Next string
+	Cost int
+}
+
+func ShortestPathInit(d *D, prefix string) *D {
+	links := d.DeclareLSet(prefix+"ShortestPathLink", ShortestPathLink{})
+	paths := d.DeclareLSet(prefix+"ShortestPath", ShortestPath{})
+
+	d.Join(links, func(link *ShortestPathLink) *ShortestPath {
+		return &ShortestPath{From: link.From, To: link.To, Cost: link.Cost}
+	}).Into(paths)
+
+	d.Join(links, paths, func(link *ShortestPathLink, path *ShortestPath) *ShortestPath {
+		if link.To != path.From {
+			return nil
+		}
+		return &ShortestPath{
+			From: link.From,
+			To:   path.To,
+			Next: link.To,
+			Cost: link.Cost + path.Cost,
+		}
+	}).Into(paths)
+
+	return d
+}
+
+func init() {
+	ShortestPathInit(NewD(""), "")
+}
+
