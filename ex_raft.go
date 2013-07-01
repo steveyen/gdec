2291cd0d93b1f2318797c7cdec2beecf982ff33b
Steve Yen <steve.yen@gmail.com>
2013-07-01 16:40:20 -0700

    More RAFT sketches.

--- ex_raft.go
+++ ex_raft.go
@@ -2,19 +2,22 @@ package gdec
 
 // Invoked by candidates to gather votes.
 type RaftVoteRequest struct {
-	CandidateTerm int    // Candidate's term.
-	CandidateAddr string // Candidate requesting vote.
-	LastLogTerm   int    // Term of candidate's last log entry.
-	LastLogIndex  int    // Index of candidate's last log entry.
+	To           string
+	From         string // Candidate requesting vote.
+	Term         int    // Candidate's term.
+	LastLogTerm  int    // Term of candidate's last log entry.
+	LastLogIndex int    // Index of candidate's last log entry.
 }
 
 type RaftVoteResponse struct {
-	FromAddr    string
-	Term        int  // Current term, for candidate to update itself.
-	VoteGranted bool // True means candidate received vote.
+	To      string
+	From    string
+	Term    int  // Current term, for candidate to update itself.
+	Granted bool // True means candidate received vote.
 }
 
 type RaftAppendEntryRequest struct {
+	To           string
 	LeaderTerm   int    // Leader's term.
 	LeaderAddr   string // So follower can redirect clients.
 	PrevLogTerm  int    // Term of log entry immediately preceding this one.
@@ -24,9 +27,10 @@ type RaftAppendEntryRequest struct {
 }
 
 type RaftAppendEntryResponse struct {
-	FromAddr    string
+	To          string
+	From        string
 	Term        int  // Current term, for leader to update itself.
-	Success     bool // True if follower contained entry matching PrevLogIndex/Term.
+	Success     bool // True if had entry matching PrevLogIndex/Term.
 	CommitIndex int
 }
 
@@ -36,10 +40,23 @@ type RaftEntry struct {
 	Entry string // Command for state machine.
 }
 
-type RaftPeer struct {
-	Addr         string
-	PrevLogIndex int
-}
+const (
+	// Lowest bits of a state are the 'kind' of a state,
+	// where ordering matters for LMax precedence.
+	state_FOLLOWER  = 0
+	state_LEADER    = 1
+	state_CANDIDATE = 2
+	state_STEP_DOWN = 3 // Must be largest for LMax precedence.
+	state_SAME      = 0 // Used to denote no change to state kind.
+
+	state_KIND_MASK    = 0x0000000f
+	state_VERSION_MASK = 0xfffffff0
+	state_VERSION_NEXT = 0x00000010
+)
+
+func stateKind(s int) int        { return s & state_KIND_MASK }
+func stateVersion(s int) int     { return s & state_VERSION_MASK }
+func stateVersionNext(s int) int { return stateVersion(s) + state_VERSION_NEXT }
 
 func RaftProtocolInit(d *D, prefix string) *D {
 	d.DeclareChannel(prefix+"RaftVoteRequest", RaftVoteRequest{})
@@ -52,10 +69,53 @@ func RaftProtocolInit(d *D, prefix string) *D {
 func RaftInit(d *D, prefix string) *D {
 	d = RaftProtocolInit(d, prefix)
 
-	// rvotes := d.Relations[prefix+"RaftVoteRequest"]
-	// rvoteresponse := d.Relations[prefix+"RaftVoteResponse"]
+	rvote := d.Relations[prefix+"RaftVoteRequest"]
+	rvoter := d.Relations[prefix+"RaftVoteResponse"]
+
 	// rappends := d.Relations[prefix+"RaftAppendEntryRequest"]
-	// rappendresponse := d.Relations[prefix+"RaftAppendEntryResponse"]
+	// rappendresponses := d.Relations[prefix+"RaftAppendEntryResponse"]
+
+	// members := d.DeclareLSet(prefix + "raftMember", "addrString")
+	// votedFor := d.DeclareLSet(prefix + "raftVotedFor", "addrString")
+	// votedForInCurrentTerm := d.DeclareLSet(prefix + "raftVotedForInCurrentTerm", "addrString")
+	// votedForInCurrentTick := d.DeclareLSet(prefix + "raftVotedForInCurrentTick", "addrString")
+
+	currentTerm := d.DeclareLMax(prefix + "raftCurrentTerm")
+	currentState := d.DeclareLMax(prefix + "raftCurrentState")
+
+	nextTerm := Scratch(d.DeclareLMax(prefix + "raftNextTerm"))
+	nextState := Scratch(d.DeclareLMax(prefix + "raftNextState"))
+
+	d.Join(currentTerm).
+		IntoAsync(nextTerm)
+	d.Join(rvote, func(r *RaftVoteRequest) int { return r.Term }).
+		IntoAsync(nextTerm)
+
+	d.Join(rvote, currentTerm, currentState, func(r *RaftVoteRequest, term, state *int) int {
+		if r.Term > *term {
+			return stateKind(*state) + state_STEP_DOWN
+		}
+		return state_SAME
+	}).Into(nextState)
+
+	d.Join(currentState, nextState, func(curr, next *int) int {
+		if *next == state_STEP_DOWN {
+			return stateVersionNext(*curr)
+		}
+		return state_SAME
+	}).IntoAsync(currentState)
+
+	d.Join(rvote, currentTerm, func(r *RaftVoteRequest, myCurrentTerm *int) *RaftVoteResponse {
+		if r.Term < *myCurrentTerm {
+			return &RaftVoteResponse{
+				To:      r.From,
+				From:    r.To,
+				Term:    *myCurrentTerm,
+				Granted: false,
+			}
+		}
+		return nil // TODO.
+	}).IntoAsync(rvoter)
 
 	// TODO.
 
